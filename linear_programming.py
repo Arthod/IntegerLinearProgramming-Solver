1218efc858aa919a916369c95c24c10b12984141
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-04-03 13:39:16 +0200

    two-phase method

--- linear_programming.py
+++ linear_programming.py
@@ -48,31 +48,30 @@ class LP:
         
         # Add SLACK and FEAS variables, and add them to constraints
         
-        for i in range(len(F_LP.constraints)):
-            slack = F_LP.add_variable(i, name="SLACK")
-            feas  = F_LP.add_variable(i, name="FEAS")
-            if F_LP.constraints[i].comparator != EQUAL:
-                F_LP.constraints[i].comparator = EQUAL
-                F_LP.constraints[i].expr_LHS += slack
-            F_LP.constraints[i].expr_LHS += feas
-            if F_LP.constraints[i].expr_RHS < 0:
-                F_LP.constraints[i].expr_LHS *= -1
-                F_LP.constraints[i].expr_RHS *= -1
+        artificial_variables = []
+        for i, constraint in enumerate(F_LP.constraints):
+            if (constraint.comparator != LESS_EQUAL):
+                artificial_variable = F_LP.add_variable(i, name="_artificial")
+                artificial_variables.append(artificial_variable)
+
+                constraint.expr_LHS += artificial_variable
+                #if constraint.expr_RHS < 0:
+                #    constraint.expr_LHS *= -1
+                #    constraint.expr_RHS *= -1
         
         
         # Objective function of feasibility LP
-        F_LP.set_objective(MAX, -sum(F_LP.variables["FEAS"].values())*self.is_maximizing)
+        F_LP.set_objective(self.is_maximizing, -sum(artificial_variables))
 
         # Solve feasibility LP and remove introduces variables
-        feasible_solition = F_LP.solve(first=True)
-        feasible_solution.pop("FEAS")
+        x_feasible = F_LP.solve(first=True)
 
         # Return if feasible
-        assert self.is_feasible(self, feasible_solution), "Didn't find feasible basis"
-        return feasible_solution
+        assert self.is_feasible(self, x_feasible), "Didn't find feasible basis"
+        return x_feasible
 
 
-    def add_variable(self, key: any, name: str="x", lb=None) -> Symbol:
+    def add_variable(self, key: any, name: str="x") -> Symbol:
         """Adds a variable to the LP
 
         Args:
@@ -90,21 +89,14 @@ class LP:
             Symbol: Sympy symbol
         """
 
-        # Add group of variable if not already registered
-        if (name not in self.variables):
-            self.variables[name] = {}
-
         # If key already in the group of the variable we raise an exception
-        if (key in self.variables[name]):
+        key = name + "_" + str(key)
+        if (key in self.variables):
             raise Exception("variable key already used")
 
         # Otherwise add the key to the variable group
-        symbol = Symbol(name + "_" + str(key))
-        self.variables[name][key] = symbol
-
-        # Add a constraint for lower bound
-        if (lb != None):
-            self.add_constraint(symbol, GREATER_EQUAL, lb)
+        symbol = Symbol(key)
+        self.variables[key] = symbol
 
         return symbol
 
@@ -134,27 +126,30 @@ class LP:
         self.is_maximizing = is_maximizing
         self.objective = expr
 
-    def solve(self, A_provided: bool=False, first: bool=False):
+    def solve(self, first: bool=False):
         self.b = np.array([constraint.expr_RHS for constraint in self.constraints])
-        self.c = np.hstack((np.array([self.is_maximizing * coeff for coeff in Poly(self.objective).coeffs()]), np.zeros(self.b.size))).astype(np.float)
+        
+        coeff_dict = self.objective.as_coefficients_dict()
+        c = []
+        for _, symbol in self.variables.items(): # TODO name here instead of "x"?
+            c.append(coeff_dict.get(symbol, 0) * self.is_maximizing)
+        self.c = np.hstack((np.array(c), np.zeros(self.b.size))).astype(np.float)
 
         self.A = []
         A_slacks = np.identity(self.b.size)
         for i, constraint in enumerate(self.constraints):
-            l = []
+            arr = []
             coeff_dict = constraint.expr_LHS.as_coefficients_dict()
-            for name in self.variables:
-                for id, symbol in self.variables[name].items(): # TODO name here instead of "x"?
-                    l.append(coeff_dict.get(symbol, 0))
-                self.A.append(l)
-                
-                if (constraint.comparator == GREATER_EQUAL):
-                    A_slacks[:,i] *= -1
+            for _, symbol in self.variables.items(): # TODO name here instead of "x"?
+                arr.append(coeff_dict.get(symbol, 0))
+            self.A.append(arr)
+            
+            if (constraint.comparator == GREATER_EQUAL):
+                A_slacks[:,i] *= -1
         self.A = np.hstack((np.array(self.A), A_slacks)).astype(np.float)
-        print("self.A, self.b, self.c")
-        print(self.A, self.b, self.c)
 
-        if first:
+
+        if (first):
             x_initial = self.get_initial_feasible_solution()
         else:
             x_initial = self.compute_feasible_basis()
@@ -174,21 +169,19 @@ class LP:
         x = np.zeros(self.c.size)
         possible_items = []
         for i in range(1, 10, 1):
-            possible_items += [i for _ in range(amount_vars)]
+            possible_items += [1 for _ in range(1)]#amount_vars)]
         permutations = itertools.permutations(possible_items, amount_vars)
-        print("permutations")
-        print(permutations)
         
+        print(self.A, self.b, self.c)
         i = 0
+
         for x_test in permutations:
             x1 = np.hstack((np.array(x_test), np.zeros(amount_vars_slack)))
-            print("\nx_test\n", x_test)
-            print("\nself.b\n", self.b)
-            print("\nself.A\n", self.A)
-            print("\nx1\n", x1)
             x = np.hstack((np.array(x_test), self.b - (self.A @ x1)))
 
             if (self.is_feasible(x)):
+                if (np.min(self.b - (self.A @ x1)) <= 0):
+                    continue
                 break
         if (not self.is_feasible(x)):
             raise Exception("No feasible solution found")
