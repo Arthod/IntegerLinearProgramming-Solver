8792d40c9c37596b913012ba3cbd5614e4f811e6
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-05-10 10:33:16 +0200

    subgradient optimization

---
+++ subgradient_optimization.py
@@ -0,0 +1,3 @@
+# Subgradient descent of the dual of the lagragian relaxed problem.
+# Gradient to non-differentiable functions
+# Find solution of the dual which gives us the lagrangian multiplier to solve the primal
